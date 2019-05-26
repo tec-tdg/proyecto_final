97f4c5dcad84829967ee5db918f1345d0fa2ead9
Fera95 <ferarios95@gmail.com>
2019-05-26 22:26:10 +0200

    Se agrega más lógica
    Se agregan más conexiones antes de modificar el Instruction memory

--- CPU/CPU/CPU_V1.sv
+++ CPU/CPU/CPU_V1.sv
@@ -17,7 +17,38 @@ logic [31:0] PC_Test_Output;
 // La siguiente estapa es el instruction memory
 // module InstructionMemory (input logic [bus-1:0] address, input logic clk, reset, output logic [bus-1:0] data);
 
+//
 InstructionMemory INSTRUCTIONMEMORY(PC_Test_Output,clk_test,reset_test,);
+
+// Se crea la salida del Instruction Memory
+
+logic [31:0] Data_out_test;
+
+// Se pasara esa salida como entrada a la ALU
+
+
+//module ALU #(parameter N=1) (input [N-1:0] a_i, b_i,input [3:0] alucontrol, output [N-1:0] result,//To display 7-segmentsoutput [3:0] output_flags);
+
+//Se quema valor a sumar el cual luego vendra ya sea de otro data_out o del extend
+
+logic [31:0] Data_Test;
+
+//Se le asigna valor a Data_Test
+
+//PONER AQUI VALOR DE 32 BITS A SUMAR
+
+//Se instancia el alucontrol para una suma 0000
+
+logic [3:0] Alu_control_test;
+
+assign Alu_control_test = 4'b0000;
+
+//Se instancia la logica de salida y de flags
+
+logic[31:0] Alu_output_test;
+logic[3:0] Alu_flags_test;
+
+ALU ALU #32(Data_out_test,Data_Test,Alu_control_test,Alu_output_test,Alu_flags_test);
 
 
 endmodule
