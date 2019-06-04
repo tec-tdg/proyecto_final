68cdaad42b66e9b1a98b897623bc0b7457c9be6e
nbaltodano <nestobaldo0412@gmail.com>
2019-06-04 16:24:34 -0600

    Added some files

---
+++ CPU/CPU/ControlUnit/Condlogic.sv
@@ -0,0 +1,28 @@
+module Condlogic(input logic clk, reset,
+					  input logic [3:0] Cond,
+					  input logic [3:0] ALUFlags,
+					  input logic [1:0] FlagW,
+					  input logic PCS,
+					  input logic RegW,
+					  input logic MemW,
+					  output logic PCSrc, 
+					  output logic RegWrite, 
+					  output logic MemWrite);
+
+	logic [1:0] FlagWrite;
+	logic [3:0] Flags;
+	logic CondEx;
+
+	FlipFlopen #(2)flagflop1(clk, reset, FlagWrite[1], ALUFlags[3:2], Flags[3:2]);
+
+	FlipFlopen #(2)flagflop20(clk, reset, FlagWrite[0], ALUFlags[1:0], Flags[1:0]);
+
+	// write controls are conditional
+	condcheck cc(Cond, Flags, CondEx);
+	
+	// Logic gates assigment
+	assign FlagWrite = FlagW & {2{CondEx}};
+	assign RegWrite = RegW & CondEx;
+	assign MemWrite = MemW & CondEx;
+	assign PCSrc = PCS & CondEx;
+endmodule
