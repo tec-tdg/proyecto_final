7daac02f444ddf4dd9ab6d73f34e83c0664f4851
nbaltodano <nestobaldo0412@gmail.com>
2019-06-10 12:25:54 -0600

    Update ControlUnit_tb.sv

--- testbench/ControlUnit_tb.sv
+++ testbench/ControlUnit_tb.sv
@@ -284,7 +284,7 @@ RegWrite). Afectado por ejecucion condicional
 //	 		#10
 	 Instru =32'b1011_001_0100_0_0101_0111_0000_0000_0001;
 	 ALUFlags = 4'b1000;
-	 		#10
+	 		#100
 //	 Instru =32'b1100_001_0100_0_0101_0111_0000_0000_0001;
 //	 ALUFlags = 4'b1000;
 //	 		#10
@@ -295,40 +295,40 @@ RegWrite). Afectado por ejecucion condicional
 //	 ALUFlags = 4'b1000;
 //	 		#10
 //	 Instru =32'b1111_001_0100_0_0101_0111_0000_0000_0001;
-	 ALUFlags = 4'b1000;
-	 #10
-	 ALUFlags = 4'b0000;
-		#10;
-	 ALUFlags = 4'b0001;
-		#10;
-	 ALUFlags = 4'b0010;
-		#10;
-	 ALUFlags = 4'b0011;
-		#10;
-	 ALUFlags = 4'b0100;
-		#10;
-	 ALUFlags = 4'b0101;
-		#10;
-	 ALUFlags = 4'b0110;
-		#10;
-	 ALUFlags = 4'b0111;
-		#10;
-	 ALUFlags = 4'b1000;
-		#10;
-	 ALUFlags = 4'b1001;
-		#10;
-	 ALUFlags = 4'b1010;
-		#10;
-	 ALUFlags = 4'b1011;
-		#10;
-	 ALUFlags = 4'b1100;
-		#10;
-	 ALUFlags = 4'b1101;
-		#10;
-	 ALUFlags = 4'b1110;
-		#10;
-	 ALUFlags = 4'b1111;
-		#10;
+//	 ALUFlags = 4'b1000;
+//	 #10
+//	 ALUFlags = 4'b0000;
+//		#10;
+//	 ALUFlags = 4'b0001;
+//		#10;
+//	 ALUFlags = 4'b0010;
+//		#10;
+//	 ALUFlags = 4'b0011;
+//		#10;
+//	 ALUFlags = 4'b0100;
+//		#10;
+//	 ALUFlags = 4'b0101;
+//		#10;
+//	 ALUFlags = 4'b0110;
+//		#10;
+//	 ALUFlags = 4'b0111;
+//		#10;
+//	 ALUFlags = 4'b1000;
+//		#10;
+//	 ALUFlags = 4'b1001;
+//		#10;
+//	 ALUFlags = 4'b1010;
+//		#10;
+//	 ALUFlags = 4'b1011;
+//		#10;
+//	 ALUFlags = 4'b1100;
+//		#10;
+//	 ALUFlags = 4'b1101;
+//		#10;
+//	 ALUFlags = 4'b1110;
+//		#10;
+//	 ALUFlags = 4'b1111;
+//		#10;
 
     $display("Valindando: B2857001");
     
